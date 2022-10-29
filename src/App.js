8dd38efd0abadb39d7a830e95c70dcf7dcb6fbab
Ali <ali.ozgur@example.com>
2022-10-29 12:13:04 +0300

    created react-router structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import {BrowserRouter ,Routes,Route} from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
 import './App.css';
+import Footer from "./components/footer/Footer"
+import Home from "./pages/Home";
+import Service from "./pages/Service";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className="App">
-     <h1>merhaba</h1>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes> 
+
+      <Route path = "/" element ={<Home/>}/>
+      <Route path = "/about" element ={<About/>}/>
+      <Route path = "/service" element ={<Service/>}/>
+      <Route path = "*" element ={<NotFound/>}/>
+     </Routes>
+     <Footer/>
+     </BrowserRouter>
     </div>
   );
 }
