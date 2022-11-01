e0d06ec7a90375b6cbc65eda67c4fbe7a7b6fc09
Ali <ali.ozgur@example.com>
2022-11-02 02:25:48 +0300

    finished sercive componet

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,2 +1,49 @@
+export const data = [
+
+
+
+    {
+            "id":1,
+            "title":"Web Tasarım",
+            "icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+    },
+
+    {
+        "id":2,
+        "title":" Mobil Uygulama",
+        "icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+},
+
+{
+    "id":3,
+    "title":" Özel Yazılımlar ",
+    "icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+},
+
+{
+    "id":4,
+    "title":"E-Ticaret",
+    "icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+},
+
+{
+"id":5,
+"title":" E-Fatura Çözümleri",
+"icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+},
+
+{
+"id":6,
+"title":"Bakım Hizmetleri ",
+"icon": "https://cdn-icons-png.flaticon.com/128/8709/8709442.png"
+
+}
+]
+
 
 
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import {data} from "../assets/data"
+import "../components/css pages/service.css"
+import Subscribe from "../components/subscribe/Subscribe"
+import Contact from "../assets/img/contact.png"
 
 const Service = () => {
   return (
-    <div>Service</div>
+<div> 
+    <Subscribe/>
+        <div className='Service'>
+
+{data.map((item)=>{
+  return (
+    <div className='items'>  <img className='img' src={item.icon} alt="icon" />
+    <p>{item.title}</p> 
+    </div>
+   
   )
+})}
+
+
+    </div>
+     <div className="bilgi">
+      <img  className='img1' src={Contact} alt="concat" /> 
+     <p className='info'>Bilgi almak için lütfen iletişime geçiniz!</p>
+      </div>
+    
+   
+    </div>
+  )
+ 
 }
 
 export default Service
